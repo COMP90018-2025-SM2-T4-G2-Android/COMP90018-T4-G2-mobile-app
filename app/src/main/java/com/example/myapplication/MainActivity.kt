f25dbb8638eb2e7504924713c7bdddd9f31f2b3c
John Kim <kakwak123@gmail.com>
2025-08-18 18:05:49 +1000

    Simple design

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var currentBalanceCents: Int = 10000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,34 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val balanceText: TextView = findViewById(R.id.balanceValue)
+        val depositButton: Button = findViewById(R.id.depositButton)
+        val withdrawButton: Button = findViewById(R.id.withdrawButton)
+
+        fun updateBalanceText() {
+            val dollars = currentBalanceCents / 100
+            val cents = currentBalanceCents % 100
+            balanceText.text = "$${dollars}.${cents.toString().padStart(2, '0')}"
+        }
+
+        updateBalanceText()
+
+        depositButton.setOnClickListener {
+            currentBalanceCents += 1000
+            updateBalanceText()
+            Toast.makeText(this, "Deposited $10", Toast.LENGTH_SHORT).show()
+        }
+
+        withdrawButton.setOnClickListener {
+            if (currentBalanceCents >= 1000) {
+                currentBalanceCents -= 1000
+                updateBalanceText()
+                Toast.makeText(this, "Withdrew $10", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Insufficient funds", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
