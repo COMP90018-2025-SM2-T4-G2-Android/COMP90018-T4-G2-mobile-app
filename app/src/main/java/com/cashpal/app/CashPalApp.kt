e10b163ca0b109967e61884df34d1db9ecc525b3
lir23o <tingnadong@gmail.com>
2025-11-06 18:22:52 +1100

    pop up

--- app/src/main/java/com/cashpal/app/CashPalApp.kt
+++ app/src/main/java/com/cashpal/app/CashPalApp.kt
@@ -5,9 +5,15 @@ import com.google.android.material.color.DynamicColors
 import com.google.firebase.FirebaseApp
 import com.cashpal.app.di.ServiceLocator
 import com.cashpal.app.utils.UserProfileCache
+import android.content.ComponentCallbacks2
 import java.io.File
+import kotlin.jvm.Volatile
 
 class CashPalApp : Application() {
+
+    @Volatile
+    private var isClearingCache = false
+
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
@@ -23,15 +29,33 @@ class CashPalApp : Application() {
         clearAppCacheAsync()
     }
 
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        if (level >= ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
+            UserProfileCache.clear()
+            clearAppCacheAsync()
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        UserProfileCache.clear()
+        clearAppCacheAsync()
+    }
+
     private fun clearAppCacheAsync() {
+        if (isClearingCache) return
+        isClearingCache = true
         Thread {
-            runCatching {
+            try {
                 clearDirectory(cacheDir)
                 externalCacheDirs?.forEach { dir ->
                     if (dir != null) {
                         clearDirectory(dir)
                     }
                 }
+            } finally {
+                isClearingCache = false
             }
         }.apply { isDaemon = true }.start()
     }
